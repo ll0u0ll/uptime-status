f8e1d61e42910614df1f71392eeed2b9551a1d37
小麻木 <52115743+ll0u0ll@users.noreply.github.com>
2022-11-18 22:34:36 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,33 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '站点监测 - lllvlll / l0u0l',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur1903154-2128f2b046aacc2f38ed5d5a',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '主页',
+      url: '#'
+    },
+    {
+      text: '博客',
+      url: 'https://www.l0u0l.com/'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/ll0u0ll'
+    },
+  ],
+};
